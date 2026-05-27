c8bebfb44e6170df1ccca980efe17a6b7a04dac4
Irish シ <irish@rustopia.gg>
2026-05-27 07:24:14 -0400

    Add entrypoint script and ProcessWrapper binary
    Add res/entrypoint.sh as the container entrypoint for running a Rust server and include a res/ProcessWrapper binary. The script sets INTERNAL_IP, ensures SteamCMD is present (downloads if missing), optionally updates and validates the Rust server when UPDATE is set, expands STARTUP variables, handles Oxide/uMod installation and sandbox disabling, adjusts LD_LIBRARY_PATH and DOTNET extraction dir, and launches the server via /ProcessWrapper with the evaluated startup command.

---
+++ res/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+if [ ! -f ./steam/steamcmd.sh ]; then
+    echo "Downloading SteamCMD"
+    curl -sSL -o ./steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+    mkdir -p ./steam
+    tar -xzf steamcmd.tar.gz -C ./steam
+    rm ./steamcmd.tar.gz
+fi
+
+if [ "${UPDATE}" == "true" ] || [ "${UPDATE}" == "1" ]; then
+    echo "Updating Main Branch"
+    # Update Rust Server
+    ./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 258550 +quit
+    # Validate Rust Server
+    ./steam/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 258550 validate +quit
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g' -e 's/\"/\\\"/g')`
+
+# OxideMod has been replaced with uMod
+if [ "${OXIDE}" == "true" ] || [ "${OXIDE}" == "1" ]; then
+    echo "Updating Oxide..."
+    curl -sSL "${OXIDE_URL}" >oxide.zip
+    unzip -o -q oxide.zip
+    rm oxide.zip
+    echo "Disabling Oxide Sandbox"
+    touch RustDedicated_Data/Managed/oxide.disable-sandbox
+fi
+
+# Fix for Rust not starting
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+
+mkdir -p /home/container/tmp
+export DOTNET_BUNDLE_EXTRACT_BASE_DIR="/home/container/tmp"
+
+echo "Starting Rust Server"
+# Run the Server
+/ProcessWrapper $MODIFIED_STARTUP
